3361e552dfbba3a27e9a04e02128bc6814ba1098
motalhakhan <mo.talhaakhan@gmail.com>
2024-06-13 06:46:28 +0530

    Arithmetic operators in java explained in single code

---
+++ arithmetic_operators.java
@@ -0,0 +1,23 @@
+class arithmetic_operators 
+{
+    public static void main(String args[])
+    {
+        int a = 16;
+        int b = 5;
+        
+        int result = a + b;// add 
+        int result2 = a - b; //subtract
+        int result3 = a * b; //multiply
+        int result4 = a / b; // it will only give quotient 
+        int result5 = a%b;  //it gives only remainder
+        
+       // same output
+        a = a +1;
+        a +=1;
+        a++; //increment operator
+
+        int result6 = ++a; // pre increment, increment the value then fetch (17 output)
+        int result7 = a++;  //post increment, fetch then increment the value (16 output)
+        System.out.println(a);
+    }
+}
