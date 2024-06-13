f8a85dd03815bd4b686c49219207a52e349a4569
motalhakhan <mo.talhaakhan@gmail.com>
2024-06-13 19:38:56 +0530

    if-else statement

---
+++ conditional_statements.java
@@ -0,0 +1,14 @@
+class conditional_statements 
+{
+    public static void main(String[] args) 
+    {
+        int a = 9;
+        int b = 8;
+        if (a>b) {
+            System.out.println("the greatest value is "+ a);
+        }
+        else{
+            System.out.println("the greatest value is " + b);
+        }
+    }
+}
