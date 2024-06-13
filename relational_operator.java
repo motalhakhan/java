f112e5247dfb025e1944d6b678245400b2692ba8
motalhakhan <mo.talhaakhan@gmail.com>
2024-06-13 07:14:25 +0530

    relational operators

---
+++ relational_operator.java
@@ -0,0 +1,15 @@
+class relational_operator 
+{
+    public static void main(String[] args) 
+    {
+        int a = 6;
+        int b = 4;
+        boolean result1 = a>b;// greater than
+        boolean result2 = a<b;// less than 
+        boolean result3 = a>=b;// greater than equal to 
+        boolean result4 = a<=b;// less than equal to 
+        boolean result5 = a==b;// equal to 
+        boolean result6= a!=b;//not equal to 
+        System.out.println(result5);
+    }
+}
