18bd87f69c8a69cc1eeab7ea9268a2838e0bcfac
MO TALHA KHAN <160721608+motalhakhan@users.noreply.github.com>
2024-06-13 06:48:40 +0530

    Update arithmetic_operators.java
    minor change

--- arithmetic_operators.java
+++ arithmetic_operators.java
@@ -8,7 +8,7 @@ class arithmetic_operators
         int result = a + b;// add 
         int result2 = a - b; //subtract
         int result3 = a * b; //multiply
-        int result4 = a / b; // it will only give quotient 
+        int result4 = a / b; // it gives only quotient 
         int result5 = a%b;  //it gives only remainder
         
        // same output
@@ -16,8 +16,8 @@ class arithmetic_operators
         a +=1;
         a++; //increment operator
 
-        int result6 = ++a; // pre increment, increment the value then fetch (17 output)
-        int result7 = a++;  //post increment, fetch then increment the value (16 output)
+        int result6 = ++a; // pre increment, increment the value then fetchthe value (17 output)
+        int result7 = a++;  //post increment, fetch first then increment the value (16 output)
         System.out.println(a);
     }
 }
