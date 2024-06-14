1ed3fa4d1dd3e0a8ab78c7cb8cba1439a31d070f
motalhakhan <mo.talhaakhan@gmail.com>
2024-06-14 12:58:41 +0530

    ternary operator

---
+++ ternary_op.java
@@ -0,0 +1,13 @@
+class ternary_op 
+{
+    public static void main(String[] args) 
+    {
+        int a = 16;
+        int b = 9;
+        // ternary operator ?: = if the statement is true run the code after ? if false then run the code after :
+        // 10 for even 20 for odd
+
+        boolean result = (a>b) ? true : false;
+        System.out.println(result);
+    }
+}
