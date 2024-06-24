fa04ec972da1d44dd4e8143d13daa013848a150d
motalhakhan <mo.talhaakhan@gmail.com>
2024-06-24 09:51:42 +0530

    do while loop

---
+++ do_while_loop.java
@@ -0,0 +1,12 @@
+public class do_while_loop
+{
+    public static void main(String[] args) 
+    {
+                int i = 5;
+                do{
+                    System.out.println("hello " + i);
+                    i++;
+                }while(i<=4);
+    }
+}
+// It will run the loop once whether the condition is true or false.
