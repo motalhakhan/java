8ac568c7193b89ddcc95fc589d96f0dffb02f33c
motalhakhan <mo.talhaakhan@gmail.com>
2024-06-24 09:53:39 +0530

    while loop

---
+++ while_loop.java
@@ -0,0 +1,12 @@
+class loop{
+    public static void main(String[] args)
+    {
+        int n = 1;
+        while (n<=5) {
+            System.out.println("hello " + n);
+            n++;
+        }
+        System.out.println("bye " + n);
+    }
+}
+// while loop runs the till the condition gives true value
